19e5f69576ce10f3eded4c7931751b2b980c6003
Ramsay <Anton@iMac-27.local>
2013-11-19 21:34:32 -0800

    new routing to pregenerate curves

--- pregenerateCurves.py
+++ pregenerateCurves.py
@@ -3,10 +3,15 @@ import sys
 
 
 def main():
+    if sys.argv[1] is None:
+        fileName = "Q.var"
+    else:
+        fileName = sys.argv[1]
     Q = []
     for i in range(100):
-        Q.append(pf.Precompute(0.05 + (i + 0.) / 100. * 1.95))
-    pf.saveVar(Q, 'Q_100.var')
+        print i
+        Q.append(pf.Precompute(0.05 + (i + 0.) / 100. * 1.45, 1000))
+    pf.saveVar(Q, fileName)
 
 
 if __name__ == '__main__':
