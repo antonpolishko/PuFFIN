7faa3e8d06f117cf01f20c30b7e4b00913710703
macmini <polishka@cs.ucr.edu>
2013-12-10 16:00:09 -0800

    code to generate toy example

---
+++ GenerateToyExample.py
@@ -0,0 +1,15 @@
+import sys
+from Puffin import *
+import numpy as np
+
+
+def main():
+    if len(sys.arg) > 1:
+        fileNameOut = sys.argv[1]
+    else:
+        fileNameOut = 'toyExample.bed'
+
+
+
+if __name__ == '__main__':
+    main()
